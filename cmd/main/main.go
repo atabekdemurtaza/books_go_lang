e7feffdb3d648e121ea5bf05a44047bd3da9dee4
atabekdemurtaza <mychannel21war@gmail.com>
2024-12-11 12:02:43 +0000

    db also modified

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -15,7 +15,7 @@ import (
 
 // Database setup
 func NewDB() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("books.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("./db/books.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
